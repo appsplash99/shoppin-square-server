a77312c79b488dc68d11876582efa3d5779cd00b
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-05-19 16:58:25 +0530

    updated: server.js with post request for populating db

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 // port will be extracted from vars
 
-const app = require('./config/expressApp')
+const app = require('./config/expressApp');
 const path = require('path'); // for dev purposes only
-const {
-  port
-} = require('./config/vars')
+const { port } = require('./config/vars');
 // db connection
 const database = require('./config/database');
 
@@ -12,14 +10,37 @@ const database = require('./config/database');
 database.connect();
 
 // Home route for testing
-// 
+//
 app.get('/', (req, res) => {
-  res.sendFile(
-    path.join(__dirname, 'views', 'home.html')
-  )
-})
+  res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
 
+/////////////////////////////////////////////////////////////
+// RUN ONCE FOR POPULATING PRODUCTS
+// const Product = require('./api/models/product.model')
+// const { PRODUCTSDATA } = require('./config/fakerDB')
+// const populatateProductsinDB = async (req, res) => {
+//   try {
+//     PRODUCTSDATA.forEach(async (product) => {
+//       const newProduct = new Product(product)
+//       const savedProduct = await newProduct.save()
+//       console.log(savedProduct)
+//     })
+//     res.status(201).json({
+//       message:"All products are successfully saved to the database"
+//     })
+//   }
+//   catch (error) {
+//     res.status(500).json({
+//           success:'false',
+//           message:"All products failed to save to the database",
+//           errorMessage: error.message
+//     })
+//   }
+// }
+// populatateProductsinDB()
+///////////////////////////////////////////////////////////
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
