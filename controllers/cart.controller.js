4f2c27af6197c0f33d11cb6ee1d7a966cf45c24b
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-06-14 18:05:25 +0530

    refactored: cart controller

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,35 +1,35 @@
-const Cart = require('../models/cart.model');
+const Cart = require('../models/cart.model')
 
 exports.getCartItems = async (req, res) => {
   try {
-    const cartItems = await Cart.find({});
+    const cartItems = await Cart.find({})
     res.status(200).json({
       success: true,
       cartItems,
-    });
+    })
   } catch (error) {
     res.status(500).json({
       success: false,
       message: 'Failed to get Cart Items',
       errorMessage: error.message,
-    });
+    })
   }
-};
+}
 
 exports.findCartItemById = async (req, res) => {
   try {
-    const cartItem = await Cart.findById(req.params.cartItemId);
+    const cartItem = await Cart.findById(req.params.cartItemId)
     res.status(200).json({
       success: true,
       cartItem,
-    });
+    })
   } catch (err) {
     res.status(400).json({
       success: false,
       message: 'Unable to retrive the cart Item',
-    });
+    })
   }
-};
+}
 
 exports.addNewCartItem = async (req, res) => {
   try {
@@ -49,41 +49,41 @@ exports.addNewCartItem = async (req, res) => {
       qty: req.body.qty,
       category: req.body.category,
       offer: req.body.offer,
-    });
-    const savedCartItem = await newCartItem.save();
+    })
+    const savedCartItem = await newCartItem.save()
     res.status(201).json({
       success: true,
       message: 'cartItem saved successfully in the database',
       savedCartItem,
-    });
+    })
   } catch (error) {
     res.status(500).json({
       success: 'false',
       errorMessage: error.message,
       message: 'Failed to save new cartItem in the database',
-    });
+    })
   }
-};
+}
 
 exports.deleteCartItemById = async (req, res) => {
   try {
     const removeCartItem = await Cart.remove({
       _id: req.params.cartItemId,
-    });
-    const newCartItems = await Cart.find();
+    })
+    const newCartItems = await Cart.find()
     res.json({
       success: true,
       deletedCartItem: removeCartItem,
       newCartItems,
-    });
+    })
   } catch (error) {
     res.json({
       success: false,
       message: 'Failed to delete cart Item',
       errorMessege: error.message,
-    });
+    })
   }
-};
+}
 
 exports.updateCartItemQtyById = async (req, res) => {
   try {
@@ -96,18 +96,18 @@ exports.updateCartItemQtyById = async (req, res) => {
           qty: req.body.qty,
         },
       }
-    );
-    const newCartItems = await Cart.find();
+    )
+    const newCartItems = await Cart.find()
     res.json({
       success: true,
       updatedCartItem,
       newCartItems,
-    });
+    })
     // console.log(updatedPrd);
   } catch (err) {
     res.json({
       message: err.message,
-    });
-    console.log(err);
+    })
+    console.log(err)
   }
-};
+}
