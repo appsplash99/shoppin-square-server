6656d56141ce1a3ad5ab3087411ae9a7230448f3
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-06-14 18:02:45 +0530

    updated: verify token middleware

---
+++ middlewares/verifyToken.middleware.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken')
+const {
+  jwtSecret
+} = require("../config/constants")
+/**
+ * To all private/protected routes
+ * pass the below function as a middleware
+ */
+
+const verify = (req, res, next) => {
+  // consuming token at each request from request headers
+  // const token = req.header('auth-token')
+
+  /** 
+   * Injected token from frontend is: 
+   * token = `Bearer ${token}`; 
+   * */
+  const token = req.headers.authorization.split(" ")[1]
+  if (!token) return res.status(401).send('Access Denied')
+
+  try {
+    // verifying token passsed by user
+    // in comparison with server's .env TOKEN_SECRET
+    const verified = jwt.verify(token, jwtSecret)
+    // console.log({ verified })
+    // inject user: userId key-value pair in request
+    req.user = verified
+    next()
+  } catch (error) {
+    res.status(400).send('Invalid Token')
+  }
+}
+
+
+module.exports = verify
