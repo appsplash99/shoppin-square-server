fae23c54d50f3e0c71c8dadb6de10b2ed973d266
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-06-28 02:31:11 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
-const app = require('./config/expressApp');
+const app = require('./config/expressApp')
 const consola = require('consola')
-const {
-  port
-} = require('./config/constants');
-const database = require('./config/database');
+const { port } = require('./config/constants')
+const database = require('./config/database')
 
 /**Connect to Mongoose */
-database.connect();
-
-// console.log(__dirname);
-
-//////////////////////////////// for dev purposes only
-const path = require('path');
-app.use('/', (req, res) => {
-  return res.sendFile(path.join(__dirname, '/views/home.html'));
-});
-//////////////////////////////// for dev purposes only
+database.connect()
 
 app.listen(port || 3001, () => {
-  consola.success(`Express Server running on port: ${port}`);
-});
+  consola.success(`Express Server running on port: ${port}`)
+})
